7afdb049053e27c02ef97fa20d551445e000778d
Domenico <10582521@polimi.it>
2022-06-09 22:50:05 +0200

    test non finiti

--- Cloud-Project/src/main/java/it/unipi/hadoop/TestBloomFilter.java
+++ Cloud-Project/src/main/java/it/unipi/hadoop/TestBloomFilter.java
@@ -2,28 +2,64 @@ package it.unipi.hadoop;
 
 import org.apache.hadoop.util.hash.MurmurHash;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.nio.file.Path;
+import java.io.*;
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class TestBloomFilter {
     //lista di filtri
-    List<int []> bloomFilters = new ArrayList<int []>();
+    ArrayList<Integer>[] bloomFilters = new ArrayList[10];
 
 
 
-    //to implement
-    public void createBloomFromFile(Path path) throws FileNotFoundException {
-        //file generato dal secondo mapReduce
-        Scanner scannerValue = new Scanner(new File("C:\\Users\\Domenico\\Desktop\\input.txt"));
-        for(int nFiltro=0;nFiltro<10;nFiltro++){
-            
+
+    public void createBloomFromFile() throws IOException {
+        // initializing
+        for (int i = 0; i < 10; i++) {
+            bloomFilters[i] = new ArrayList<Integer>();
         }
+        //file generato dal secondo mapReduce
+        //File myObj = new File("C:\\Users\\Domenico\\Desktop\\Cloud-Project-2nd-Impl\\Cloud-Project\\src\\main\\java\\it\\unipi\\hadoop\\part-r-00000.txt");
+        //Scanner scannerValue = new Scanner(myObj);
+        BufferedReader objReader = null;
 
 
+        try {
+            objReader = new BufferedReader(new FileReader("C:\\Users\\Domenico\\Desktop\\Cloud-Project-2nd-Impl\\Cloud-Project\\src\\main\\java\\it\\unipi\\hadoop\\part-r-00000"));
+            String strCurrentLine = objReader.readLine();
+            int i=0;
+            while (strCurrentLine != null) {
+                //String line = objReader.readLine();
+                String myArray1[] = strCurrentLine.split("\t");
+                String myArray2[] = myArray1[1].split(" ");
+                for (int j = 0; j < myArray2.length; j++) {
+                    //int index = (int) Double.parseDouble(myArray1[0]);
+                    //this.bloomFilters[index-1].add(Integer.parseInt(myArray2[j]));
+                    this.bloomFilters[i].add(Integer.parseInt(myArray2[j]));
+                }
+                i++;
+                strCurrentLine = objReader.readLine();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        } finally {
+
+        try {
+            if (objReader != null)
+                objReader.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }}
+
+
+        for(int l=0;l<10;l++){
+            System.out.println("bloom "+l+" :"+"\n");
+            for(int j=0;j<bloomFilters[l].size();j++){
+                //System.out.println(bloomFilters[l].get(j)+" ");
+
+            }
+        }
     }
 
     public int isMemberBloom(int filter[], String id_film ){
@@ -41,6 +77,7 @@ public class TestBloomFilter {
 
 
     public void test () throws FileNotFoundException {
+        /*
         //FP
         MurmurHash hasher = new MurmurHash();
         //file input
@@ -62,9 +99,9 @@ public class TestBloomFilter {
             //testo su tutti i filtri tranne quello a cui appartiene davvero
             for(int i=0;i<9;i++){
                 if((ratingInt-1)!=i){
-                    if(isMemberBloom(this.bloomFilters.get(i),tokens[0])==1  ){
-                        FP[ratingInt-1]++;
-                    }
+                    //if(isMemberBloom(this.bloomFilters[i],tokens[0])==1  ){
+                     //   FP[ratingInt-1]++;
+                   // }
                 }
 
             }
@@ -72,18 +109,22 @@ public class TestBloomFilter {
         }
 
 
+         */
+
         //TN
         //file generato dal primo mapreduce
-        Scanner scannerResultReducer = new Scanner(new File("C:\\Users\\Domenico\\Desktop\\demofile.txt"));
+        Scanner scannerResultReducer = new Scanner(new File("C:\\Users\\Domenico\\Desktop\\Cloud-Project-2nd-Impl\\Cloud-Project\\src\\main\\java\\it\\unipi\\hadoop\\part-r-00001"));
         int realValue[] = new int[10];
+        int m=0;
         while (scannerResultReducer.hasNextLine()) {
-            String line = scannerFilm.nextLine();
-            String[] tokens = new String[2];
-            tokens[0] = line.split("\t")[0]; //key
-            tokens[1] = line.split("\t")[1]; //number of film in associated rating
-            int keyInt = Integer.parseInt(tokens[0]);
-            realValue[keyInt]=Integer.parseInt(tokens[1]);
+            String line = scannerResultReducer.nextLine();
+            String myArray1[] = line.split("\t");
+            //int keyInt = Integer.parseInt(tokens[0]);
+            realValue[m]=Integer.parseInt(myArray1[1]);
+            m++;
         }
+
+        //to implement
         int[] TN = new int[10];
         int tot = 1247687;
         for (int i=0;i<10;i++){
@@ -93,8 +134,9 @@ public class TestBloomFilter {
         //FPR=FP/(FP+TN)
         double fpr[] = new double[10];
         for(int i=0;i<10;i++){
-            fpr[i] = FP[i]/(FP[i]+TN[i]);
-            System.out.println(fpr[i]);
+            //fpr[i] = FP[i]/(FP[i]+TN[i]);
+            //System.out.println(fpr[i]);
+            System.out.println(TN[i]);
         }
 
 
